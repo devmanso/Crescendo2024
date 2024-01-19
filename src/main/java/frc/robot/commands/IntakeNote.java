00152561a1248d57be23cbccc5b482494f7ddca8
devmanso <mansourquddus@protonmail.com>
2024-01-19 16:47:05 -0500

    IntakeCommand todo's

--- src/main/java/frc/robot/commands/IntakeNote.java
+++ src/main/java/frc/robot/commands/IntakeNote.java
@@ -5,24 +5,36 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 
 public class IntakeNote extends Command {
+  
+  private Intake intake = new Intake();
+  
   /** Creates a new IntakeNote. */
-  public IntakeNote() {
+  public IntakeNote(Intake intake) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.intake = intake;
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    intake.stop();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    // grab the note in
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    // stop the intake
+  }
 
   // Returns true when the command should end.
   @Override
