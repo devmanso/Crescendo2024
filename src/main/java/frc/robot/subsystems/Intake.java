b256d6f2a66f2efda572dbd13dcedde1193aacaa
devmanso <mansourquddus@protonmail.com>
2024-01-19 16:10:16 -0500

    intake theoretical

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  private TalonFX bottomMotor = new TalonFX(10);
+  private TalonFX topMotor = new TalonFX(11);
+  private DigitalInput noteChecker = new DigitalInput(0);
+  
+  // write a function here that returns true if the limit switch (noteChecker) is clicked or pressed
+  // and false if it is not
+
+  /**
+   * sets motors to 50 percent
+   */
+  public void grabNote() {
+    bottomMotor.set(.5); // 50 percent
+    topMotor.set(.5);
+  }
+
+  /**
+   * sets motors to -50 percent
+   */
+  public void spitNote() {
+    bottomMotor.set(-.5);
+    topMotor.set(-.5);
+  }
+
+  /** Creates a new Intake. */
+  public Intake() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
