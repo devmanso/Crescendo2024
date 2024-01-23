0b120fef9f5f7acc648bdf383d65437f7efdfa88
devmanso <mansourquddus@protonmail.com>
2024-01-23 10:39:19 -0700

    compressor 1/2

---
+++ src/main/java/frc/robot/subsystems/AirCompressor.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AirCompressor extends SubsystemBase {
+
+  private Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+
+  public void enableCompressor() {
+    compressor.enableDigital();
+  }
+
+  public void disableCompressor() {
+    compressor.disable();
+  }
+
+  public void displayCompressorData() {
+    boolean enabled = compressor.isEnabled();
+    boolean pressureSwitch = compressor.getPressureSwitchValue();
+    double pressure = compressor.getPressure();
+    double current = compressor.getCurrent();
+    
+    SmartDashboard.putBoolean("Compresser enabled: ", enabled);
+    SmartDashboard.putNumber("Compressor Current", current);
+    SmartDashboard.putNumber("Pressure: ", pressure);
+    SmartDashboard.putBoolean("Pressure Switch: ", pressureSwitch);
+  }
+
+  /** Creates a new AirCompressor. */
+  public AirCompressor() {}
+
+  @Override
+  public void periodic() {
+    displayCompressorData();
+  }
+}
