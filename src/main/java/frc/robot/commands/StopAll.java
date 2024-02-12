6abe6f3f616daf1c8c1cd26090213e120fc2cc54
devmanso <mansourquddus@protonmail.com>
2024-02-12 15:05:28 -0500

    code cleanup

--- src/main/java/frc/robot/commands/StopAll.java
+++ src/main/java/frc/robot/commands/StopAll.java
@@ -5,11 +5,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
-public class StopAll extends Command {
+public class StopAll extends InstantCommand {
+
   private Shooter shooter;
   private Feeder feeder;
   private Intake intake;
@@ -20,28 +22,14 @@ public class StopAll extends Command {
     this.intake = intake;
 
     addRequirements(shooter, feeder, intake);
-    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
+  public void initialize() {
     shooter.stopShooter();
     feeder.stopFeeder();
     intake.stop();
   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return true;
-  }
 }
