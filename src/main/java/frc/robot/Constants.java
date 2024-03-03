7596693906192751e7148c3b12c9bc569c805cfd
devmanso <mansourquddus@protonmail.com>
2024-03-03 10:44:12 -0500

    more code cleanup in Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,18 +69,6 @@ public final class Constants {
                 public static final int LIMITSWITCH_ID = 0;
         }
 
-        public static final class ModuleConstants {
-                // TODO: ask mechanical about these
-                public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
-                public static final double kDriveMotorGearRatio = 1 / 5.8462;
-                public static final double kTurningMotorGearRatio = 1 / 18.0;
-                public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
-                public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
-                public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
-                public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
-                public static final double kPTurning = 0.5;
-        }
-
         public static final class OIConstants {
                 public static final int kDriverControllerPort = 0;
 
